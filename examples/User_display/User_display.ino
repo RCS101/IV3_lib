2ffb58eb067199cb10eea8f61bfd7b20bb0f0ba9
Robin Sterling <robincsterling@gmail.com>
2020-05-03 21:30:46 +0100

    Improved user_display demo

--- examples/User_display/User_display.ino
+++ examples/User_display/User_display.ino
@@ -1,5 +1,6 @@
 #include <IV3.h>
-
+// The IV3 clock displays bitfields
+// Here is the definition for the numbers
 #define ZERO  0x3F
 #define ONE   0x06
 #define TWO   0x5B
@@ -14,26 +15,34 @@
 
 IV3_clock IV3;
 
+// Tubes numbers start left to right.
 uint8_t tubes[4];
-uint16_t cnt = 100;
+uint16_t cnt = 9999;
 int k =0, j;
 
 void setup()
 {
   Serial.begin(9600);
+
 }
 
 void loop()
 {
+  // Set clock in user_display mode
+  // The clock will stay in this mode until either .clock_mode() is called
+  // or no I2C commands are issued for 10seconds
+  // tubes is an array with our 4 bitfields to be displayed
   IV3.user_display(tubes);
-  delay(200);
+  // Crude countdown,change number every second. Better to do this by reading to time from the IV3 clock
+  delay(100);
 
+  // Cycle along the DP on the tubes
   for(j=0;j<4;j++)
   {
     if(j==k)
       tubes[j] |= DP;
     else
-      tubes[j] &= 0x7F;
+      tubes[j] &= 0x7F; // Remove DP
   }
 
   k++;
@@ -41,16 +50,17 @@ void loop()
   {
     cnt--;
     k=0;
-    write_IV3_num(cnt, tubes);
+    write_IV3_num(cnt, tubes); // Set the next number to be displayed
   }
 
   if (cnt == 0)
   {
-    IV3.clock_mode();
+    IV3.clock_mode(); // If we've finished the countdown go back to clock mode
     while (1);
   }
 }
 
+// breaks a 4 digit number into 4 separate ints
 void numberWang(uint16_t numb, uint8_t *numbers)
 {
   numbers[0] = numb / 1000;
@@ -67,6 +77,7 @@ void numberWang(uint16_t numb, uint8_t *numbers)
   Serial.println("");
 }
 
+// Converts the number into the correct bitfield
 void write_IV3_num(uint16_t numb, uint8_t *t)
 {
   uint8_t numbers[4];
